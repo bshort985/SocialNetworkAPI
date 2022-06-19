52e1b5cd27bf154bfd4c86592f7b6f7b02988495
bshort985 <97572888+bshort985@users.noreply.github.com>
2022-06-18 21:44:27 -0400

    created user model

---
+++ models/User.js
@@ -0,0 +1,41 @@
+// user model. using validator to validate email. thanks stack overflow!
+
+const { Schema, model } = require("mongoose");
+import { isEmail } from "validator";
+
+
+const userSchema = new Schema (
+    {
+        userName: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: { validator: isEmail , message: "Invalid Email"}
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: "Thought"
+        }],
+        friends: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+            },
+        ],
+    },
+    {
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+    }
+);
+
+const User = model("User", userSchema);
+module.exports = User;
